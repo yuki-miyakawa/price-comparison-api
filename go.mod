fc35a65428173fa610c31bee896080a6393066a7
y-miyakaw <y-miyakawa@tribeat.com>
2024-04-12 19:42:26 +0900

    update main.go and go modules

--- go.mod
+++ go.mod
@@ -3,19 +3,11 @@ module github.com/y-miyakaw/price-comparison-api
 go 1.20
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo v3.3.10+incompatible
-	gorm.io/driver/postgres v1.5.4
-	gorm.io/gorm v1.25.5
+	github.com/lib/pq v1.10.9
 )
 
 require (
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.3 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"github.com/y-miyakaw/price-comparison-api/cmd/login"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/y-miyakaw/price-comparison-api/internal/controller"
+	"github.com/y-miyakaw/price-comparison-api/internal/repository"
+	"github.com/y-miyakaw/price-comparison-api/internal/router"
+	"github.com/y-miyakaw/price-comparison-api/internal/usecase"
 )
 
 func main() {
-	user := login.CreateJWT()
-	login.ParseJWT(user)
-
+	// godotenv.Load(".env.local")
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("error opening db: %v", err)
+	}
+	log.Println(db)
+	productRepository := repository.NewProductRepository(db)
+	productUsecase := usecase.NewProductUsecase(productRepository)
+	productController := controller.NewProductController(productUsecase)
+	e := router.NewRouter(productController)
+	e.Logger.Fatal(e.Start(":8080"))
 }
